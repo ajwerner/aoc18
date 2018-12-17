1a9c618bbe8b72ef9c34b7998c58caa7f69d9346
Andrew Werner <awerner32@gmail.com>
2018-12-17 03:04:53 +0000

    add 5

--- src/aoc18/core.clj
+++ src/aoc18/core.clj
@@ -4,6 +4,7 @@
    [aoc18.day2 :as day2]
    [aoc18.day3 :as day3]
    [aoc18.day4 :as day4]
+   [aoc18.day5 :as day5]
    [aoc18.inputs :as inputs])
   (:gen-class))
 
@@ -16,4 +17,6 @@
   (println "3.1) " (day3/overlapping-claims inputs/day3))
   (println "3.2) " (day3/unoverlapping-claim inputs/day3))
   (println "4.1) " (day4/best-spot inputs/day4))
-  (println "4.2) " (day4/minute-most-asleep inputs/day4)))
+  (println "4.2) " (day4/minute-most-asleep inputs/day4))
+  (println "5.1) " (day5/reacted-len inputs/day5))
+  (println "5.2) " (day5/shortest-with-elimination inputs/day5)))
---
+++ src/aoc18/day5.clj
@@ -0,0 +1,44 @@
+(ns aoc18.day5
+  (:require
+   [clojure.string :refer [upper-case]]))
+
+(defn react [p]
+  (loop [head (list) cur p]
+    (let [[v1 v2] (take 2 cur)] 
+      (if (nil? v2)
+        (concat (reverse head) cur)
+        (let  [upper-1 (Character/isUpperCase v1)
+               upper-2 (Character/isUpperCase v2)
+               match (and (not= upper-1 upper-2)
+                          (= (upper-case v1) (upper-case v2)))]
+          (if match
+            (if (> (count head) 0)
+              (recur (rest head) (conj (drop 2 cur) (first head)))
+              (recur head (drop 2 cur)))
+            (recur (conj head v1) (conj (rest cur)))))))))
+
+(defn filter-unit [lower-case]
+  (let [uc (Character/toUpperCase lower-case)]
+    (fn [c]
+      (not (or (= c uc) (= c lower-case))))))
+
+(defn react-polymer [polymer]
+  (apply str (react (seq polymer))))
+
+(defn reacted-len [polymer]
+  (count (react (seq polymer))))
+
+
+(def letters 
+  (seq "abcdefghijklmnopqrstuvwxyz"))
+
+(defn shortest-with-elimination [polymer]
+  (let [s (seq polymer)]
+    (reduce 
+     (fn [min-so-far to-eliminate]
+       (let [with-removed (react (filter (filter-unit to-eliminate) s))
+             n (count with-removed)]
+         (if (<  n min-so-far) n min-so-far)))
+     (count polymer)
+     letters)))
+
